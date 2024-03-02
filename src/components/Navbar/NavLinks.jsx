e7fc000be021288985e0ff16e07eb674c12c4234
raviw45 <ravikantwaghmare82@gmail.com>
2024-03-02 15:26:26 +0530

    Customizations on css

--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -10,7 +10,7 @@ const NavLinks = () => {
     <>
       {links.map((link) => (
         <div key={link.name}>
-          <div className=" text-left my-2 md:my-0 md:bg-none md:cursor-pointer  group">
+          <div className=" font-bold text-left my-2 md:my-0 md:bg-none md:cursor-pointer  group">
             <h1
               className="flex justify-between items-center md:pr-0 pr-4 group  px-2 py-1 rounded-lg hover:bg-slate-500
               "
@@ -43,17 +43,17 @@ const NavLinks = () => {
                     mt-1 bg-white rotate-45"
                     ></div>
                   </div>
-                  <div className="bg-slate-700 rounded-lg p-2 text-white md:h-full h-[20rem] overflow-scroll md:overflow-hidden">
+                  <div className="bg-white rounded-lg p-2 text-black md:h-full h-[20rem] overflow-scroll md:overflow-hidden">
                     {link.sublinks.map((mysublinks) => (
                       <div key={mysublinks.name} className=" grid md:grid-cols-2 gap-2 md:p-2 my-2 ">
                         {mysublinks.sublink.map((slink) => (
-                          <span key={slink.name} className="text-sm text-gray-600 border-gray-800 hover:bg-gray-700 hover:bg-opacity-60 rounded-lg p-2">
+                          <span key={slink.name} className="text-sm text-gray-600 border-gray-800 hover:bg-slate-300 hover:bg-opacity-60 rounded-lg p-2">
                             <NavLink
                             style={({isActive})=>(isActive?{color:'violet'}:{color:"white"})} 
                               to={slink.link}
                               className="hover:text-purple-500"
                             >
-                              <h1 className=" text-white">{slink.name}</h1>
+                              <h1 className=" text-black">{slink.name}</h1>
                               <p className="text-xs text-gray-500">
                                 {slink.desc}
                               </p>
