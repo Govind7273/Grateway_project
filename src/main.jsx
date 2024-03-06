ac3b873726107ab1766d1e4e2cfdb2178654d0cd
raviw45 <ravikantwaghmare82@gmail.com>
2024-03-06 16:34:37 +0530

    Chat bot added

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import Chatbot from "./components/Chatbot.jsx";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -10,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Chatbot />
     </QueryClientProvider>
   </BrowserRouter>
   // </React.StrictMode>,
