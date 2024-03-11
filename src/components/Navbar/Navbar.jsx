4c90ad1c1cff226f3841604606d63d7659d33359
sagar yenkure <yenkuresagar2104@gmail.com>
2024-03-11 15:41:58 +0530

    Nav_update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,103 +1,152 @@
-
-
-import { useState } from "react";
-import { IoClose, IoMenu } from "react-icons/io5";
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { links } from "./Mylinks";
 import { NavLink } from "react-router-dom";
 import GTW from "./images/GTW.png";
-
-import { useNavigate } from "react-router-dom";
-import NavLinks from "./NavLinks";
+import { Ham } from "./Ham";
 
 const Navbar = () => {
-  const navigate = useNavigate();
-  const [open, setOpen] = useState(false);
+  const [showNav, setShowNav] = useState(false);
+  const HandleNav = (value) => {
+    setShowNav(value);
+  };
 
   return (
-    <nav className="md:bg-white  bg-white  font-headingFont font-extrabold w-full md:fixed z-50">
-      <div className="flex items-center font-medium justify-around">
-        <div className="z-50  md:w-auto w-full text-white flex justify-between">
-          <img
-            onClick={() => navigate("/")}
-            src={GTW}
-            alt="Vionsys"
-            className="md:cursor-pointer"
-          />
-          <div
-            className="text-3xl md:hidden transition-all duration-200 flex justify-center items-center pr-6"
-            onClick={() => setOpen((prevOpen) => !prevOpen)}
-          >
-            {!open ? <IoMenu color="black" /> : <IoClose color="black" />}
-          </div>
-        </div>
-
-        <div
-          className={`${
-            open ? "block" : "hidden"
-          } md:flex flex-col pl-6 md:pl-0 z-50 p-5 bg-white md:bg-white gap-4 absolute top-16 w-full transition-all 0.2s ease-in md:w-fit md:top-0 md:static justify-center md:flex-row md:items-center md:gap-4`}
-        >
-          <div className="flex flex-col md:flex-row justify-center">
-            <NavLink
-              style={({ isActive }) =>
-                isActive ? { color: "violet" } : { color: "black" }
-              }
-              to="/"
-              className="hover:bg-violet-200  font-bold text-black px-6 py-1 rounded-full "
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/AboutUs"
-              style={({ isActive }) =>
-                isActive ? { color: "violet" } : { color: "black" }
-              }
-              className="hover:bg-violet-200 font-bold text-black px-6 py-1 rounded-full "
-            >
-              About Us
-            </NavLink>
-
-            <NavLinks />
-            <NavLink
-              to="/Indrustry"
-              style={({ isActive }) =>
-                isActive ? { color: "violet" } : { color: "black" }
-              }
-              className="hover:bg-violet-200 font-bold text-black px-6 py-1 rounded-full "
-            >
-              
-              Industries
-            </NavLink>
-
-            <NavLink
-              to="/Career"
-              style={({ isActive }) =>
-                isActive ? { color: "violet" } : { color: "black" }
-              }
-              className="hover:bg-violet-200 font-bold text-black px-6 py-1 rounded-full"
-            >
-              Career
-            </NavLink>
-            <NavLink
-              to="/Intership"
-              style={({ isActive }) =>
-                isActive ? { color: "violet" } : { color: "black" }
-              }
-              className="hover:bg-violet-200 font-bold text-black px-6 py-1 rounded-full"
-            >
-              Internship
-            </NavLink>
-            <NavLink
-              to="/ContactUs"
-              style={({ isActive }) =>
-                isActive ? { color: "violet" } : { color: "black" }
-              }
-              className="hover:bg-violet-200 font-bold text-black px-6 py-1 rounded-full "
-            >
-              Contact
-            </NavLink>
-          </div>
-        </div>
+    <header className="flex gap-5 bg-white z-50 justify-between px-2 md:justify-evenly fixed w-full ">
+      <div className="logo">
+        <NavLink to={"/"}>
+          <img src={GTW} alt="GreatWay" />
+        </NavLink>
       </div>
-    </nav>
+      <span className="md:hidden">
+        <Ham setshowNav={HandleNav} showNav={showNav} />
+      </span>
+      <AnimatePresence>
+        <motion.nav
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -50 }}
+          transition={{ duration: 0.3, ease: "easeOut" }} // Added transition
+          className={`${
+            showNav ? "visible" : "hidden"
+          } md:visible nav flex flex-col md:flex-row items-center justify-center md:justify-end md:gap-6 absolute md:static top-20 bg-white w-full -left-[2px] md:w-auto md:flex md:items-center`}
+        >
+          <NavLink
+            style={({ isActive }) =>
+              isActive ? { color: "violet" } : { color: "black" }
+            }
+            className="hover:bg-gray-300 p-2 rounded-xl font-bold"
+            to={"/"}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            style={({ isActive }) =>
+              isActive ? { color: "violet" } : { color: "black" }
+            }
+            className="hover:bg-gray-300 p-2 rounded-xl font-bold"
+            to={"/AboutUs"}
+          >
+            About Us
+          </NavLink>
+          <FlyoutLink FlyoutContent={PricingContent} className="text-black ">
+            <NavLink className="hover:bg-gray-300 p-2 rounded-xl font-bold text-black">
+              Services
+            </NavLink>
+          </FlyoutLink>
+          <NavLink
+            style={({ isActive }) =>
+              isActive ? { color: "violet" } : { color: "black" }
+            }
+            className="hover:bg-gray-300 p-2 rounded-xl font-bold"
+            to={"/Indrustry"}
+          >
+            Industries
+          </NavLink>
+          <NavLink
+            style={({ isActive }) =>
+              isActive ? { color: "violet" } : { color: "black" }
+            }
+            className="hover:bg-gray-300 p-2 rounded-xl font-bold"
+            to={"/Career"}
+          >
+            Career
+          </NavLink>
+          <NavLink
+            style={({ isActive }) =>
+              isActive ? { color: "violet" } : { color: "black" }
+            }
+            className="hover:bg-gray-300 p-2 rounded-xl font-bold"
+            to={"/Intership"}
+          >
+            Internship
+          </NavLink>
+          <NavLink
+            style={({ isActive }) =>
+              isActive ? { color: "violet" } : { color: "black" }
+            }
+            className="hover:bg-gray-300 p-2 rounded-xl font-bold"
+            to={"/ContactUs"}
+          >
+            Contact
+          </NavLink>
+        </motion.nav>
+      </AnimatePresence>
+    </header>
+  );
+};
+
+const FlyoutLink = ({ children, href, FlyoutContent }) => {
+  const [open, setOpen] = useState(false);
+  const showFlyout = FlyoutContent && open;
+
+  return (
+    <div
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      className="relative w-fit h-fit"
+    >
+      <a href={href} className="relative flex items-center text-white">
+        {children}
+        <span
+          style={{
+            transform: showFlyout ? "scaleX(1)" : "scaleX(0)",
+          }}
+          className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-indigo-300 transition-transform duration-300 ease-out"
+        />
+      </a>
+      <AnimatePresence>
+        {showFlyout && (
+          <motion.div
+            initial={{ opacity: 0, y: 15 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 15 }}
+            style={{ translateX: "-50%" }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className="absolute left-[60%] top-[52px] z-50  md:top-[3.6rem] bg-white text-black"
+          >
+            <FlyoutContent />
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+const PricingContent = () => {
+  return (
+    <div className="md:w-[50rem] bg-white overflow-y-scroll h-[20rem] md:h-full w-[21rem] gap-3 md:overflow-hidden flex flex-col p-4  md:grid md:grid-cols-2 shadow-lg rounded-lg">
+      {links.map((l) => {
+        return (
+          <div className="hover:bg-gray-200 rounded-xl p-2">
+            <NavLink to={l.link}>
+              <h1 className="font-bold w-fit">{l.name}</h1>
+              <span className="text-xs">{l.desc}</span>
+            </NavLink>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
