21b1d6de305a326930bfe8f90e57224153dd53fd
Rutuja-Vionsys <Rutujakerkar@vionsys.com>
2024-05-13 17:52:00 +0530

    Changes made in UI of About us page , content changed and images changed in About us page.

--- src/components/AboutUs/SecondSection.jsx
+++ src/components/AboutUs/SecondSection.jsx
@@ -1,6 +1,6 @@
  export const SecondSection = () => {
     return (
-      <div className="flex w-[90%]  flex-wrap gap-10 p-6 ">
+      <div className="flex w-[90%]  flex-wrap gap-10 p-6 mt-10 ">
         {/* first left part */}
         {/* <div className="flex-2 text-white">
           <img
@@ -10,14 +10,14 @@
           />
         </div> */}
         {/* right side part */}
-        <div className="flex-1 w-[50%] items-center flex justify-center flex-col gap-10  ">
+        <div className="flex-1 w-[90%] md:w-[50%] items-center flex justify-center flex-col gap-5 md:gap-10 mt-10 ">
           {/* heading of component */}
-          <h1 className="text-4xl text-center p-4 font-bold  font-headingFont text-violet-400">
+          <h1 className="text-2xl md:text-4xl text-center p-0 md:p-4  font-headingFont text-violet-900 cursor-pointer">
             Our Corporate Social Responsibility
           </h1>
 
           {/* paragraph */}
-          <p className="text-md text-start font-navlistFont text-slate-400 font-[500]">
+          <p className="text-md md:text-xl text-center font-navlistFont text-slate-900 font-[500] cursor-pointer">
             Corporate Social Responsibility (CSR) is a crucial aspect for IT
             companies, as they have the opportunity to leverage their
             technological expertise and influence to make a positive impact on
