a5e8a1dc263f15151d0c02b2810581f67e88a950
Vansh-Vionsys <Technowebdev045454>
2024-05-16 17:28:17 +0530

    COntactUs updated

--- src/components/ContactUs/ContactForm.jsx
+++ src/components/ContactUs/ContactForm.jsx
@@ -1,3 +1,6 @@
+
+/***************************************/
+
 import { useRef, useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import emailjs from "@emailjs/browser";
@@ -6,6 +9,7 @@ import { ContactScheama } from "../../Yupschema/ContactUsScheama";
 import ReCAPTCHA from "react-google-recaptcha";
 import { contactEmail } from "../../functions/EmailSendFunction";
 import { useCareerForm } from "../../utils/useCareerForm";
+
 
 const ContactForm = () => {
   const captch = useRef();
@@ -20,50 +24,80 @@ const ContactForm = () => {
     message: "",
   });
 
-  // contact form funnction
   const InputBox = (name, type, error) => {
-    const placeholder = ("Enter Your " + name).toString().toUpperCase();
+    const placeholder = ("Enter Your " + name).toString();
     return (
-      <div>
-        <input
-          type={type}
-          id="name"
-          name={name}
-          required
-          placeholder={placeholder}
-          value={ContactDetails[`${name.toLowerCase()}`]}
-          onChange={(e) =>
-            setContactDetails({
-              ...ContactDetails,
-              [e.target.name]: e.target.value,
-            })
-          }
-          className="mt-4 md:w-[20rem] w-full p-3 bg-slate-600 bg-opacity-30 text-white rounded-md flex  border-violet-500 border-l-4 shadow-md"
-        />
-        {error ? <div className="text-red-500 text-xs">{error}</div> : ""}
+      <div className="bg-white p-4 rounded-lg">
+        <div className="relative bg-inherit ">
+          <input
+            type={type}
+            id={name}
+            name={name}
+            required
+            className="peer bg-transparent h-10 lg:w-72 w-full rounded-xl text-black placeholder-transparent ring-2 px-2 ring-gray-700 focus:ring-sky-600 focus:outline-none focus:border-rose-600 transition-all"
+            placeholder={placeholder}
+            value={ContactDetails[`${name.toLowerCase()}`]}
+            onChange={(e) =>
+              setContactDetails({
+                ...ContactDetails,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+          <label
+            htmlFor={name}
+            className="absolute cursor-text left-2 -top-3 text-sm text-gray-500 bg-inherit px-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all"
+          >
+            {placeholder}
+          </label>
+          {error ? (
+            <div className="text-red-500 text-xs">{error}</div>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     );
   };
+
+
   // message are of the form
   const Textarea = (message, error) => {
     return (
+
       <>
-        <textarea
-          id="message"
-          name={message}
-          rows="4"
-          value={ContactDetails[message]}
-          placeholder="DESCRIPTION"
-          onChange={(e) =>
-            setContactDetails({
-              ...ContactDetails,
-              [e.target.name]: e.target.value,
-            })
-          }
-          className=" mt-4 p-2 bg-slate-600 bg-opacity-30 text-white rounded-md border-violet-500 border-l-4 shadow-md"
-        ></textarea>
-        {error ? <div className="text-red-500 text-xs">{error}</div> : ""}
+        <div class="bg-white p-4 rounded-lg">
+          <div class="relative bg-inherit">
+            <textarea
+              id="message"
+              name={message}
+              rows="4"
+              value={ContactDetails[message]}
+
+              onChange={(e) =>
+                setContactDetails({
+                  ...ContactDetails,
+                  [e.target.name]: e.target.value,
+                })
+              }
+              className="peer bg-transparent h-32 w-full 
+         rounded-xl text-gray-800 placeholder-transparent 
+        ring-2 px-4 py-2 ring-gray-700 focus:ring-sky-600 
+        focus:outline-none focus:border-rose-600"
+              placeholder="Description"
+            ></textarea>
+            <label for="message" class="absolute cursor-text 
+         left-4 -top-3 text-sm text-gray-500 bg-inherit 
+         px-1 peer-placeholder-shown:text-base 
+         peer-placeholder-shown:text-gray-500 
+         peer-placeholder-shown:top-2 peer-focus:-top-3 
+         peer-focus:text-sky-600 peer-focus:text-sm 
+         transition-all">Description</label>
+            {error ? <div className="text-red-500 text-xs">{error}</div> : ""}
+          </div>
+        </div>
       </>
+
     );
   };
 
@@ -112,52 +146,59 @@ const ContactForm = () => {
       <Toaster />
       <form
         onSubmit={(e) => handlesendQuery(e)}
-        className=" md:w-[60%] justify-center flex gap-2 flex-col w-full font-navlistFont flex-1 px-4 py-6"
+        className="md:w-[60%] justify-top flex gap-2 flex-col w-full font-navlistFont flex-1 px-4 py-6"
       >
-        <h1 className="text-5xl font-headingFont  font-extrabold text-violet-600 flex justify-center mb-0 mt-4">
-          Get in touch with us
+        <h1 className="text-4xl font-headingFont  font-extrabold text-sky-500 flex justify-center">
+          Send us message
         </h1>
-        <div className="flex w-full md:gap-8 flex-col md:flex-row">
+        <h1 className="text-xl font-headingFont text-gray-700 flex justify-center mb-0 ">Our friendly team would love to hear from you.</h1>
+        <div className="flex w-full md:gap-8 flex-col md:flex-row justify-center">
           {/* Your Full name input box */}
           {InputBox("name", "text", error.name)}
           {/* Your Email name input box */}
           {InputBox("email", "email", error.email)}
         </div>
-        <div className="flex md:gap-8 w-full flex-col md:flex-row ">
+        <div className="flex md:gap-8 w-full flex-col md:flex-row justify-center">
+
           {/* number number field */}
           {InputBox("number", "text", error.number)}
-          <div className="">
-            <div
-              className="flex flex-col p-3 mt-4 w-full md:w-[20rem]  bg-slate-600 bg-opacity-30 text-black rounded-md border-violet-500 border-l-4 shadow-md
-"
-            >
-              <select
-                onChange={(e) =>
-                  setContactDetails({
-                    ...ContactDetails,
-                    [e.target.name]: e.target.value,
-                  })
-                }
-                id="subject"
-                name="subject"
-                className="bg-transparent text-white"
-              >
-                <option className="text-white hidden" value="">
-                  How can we Help ?
-                </option>
-                <option className="text-black" value="About Service">
-                  About Service
-                </option>
-                <option className="text-black" value="Technical Question">
-                  Technical Question
-                </option>
-                <option className="text-black" value="Career">
-                  Career
-                </option>
-                <option className="text-black" value="Something Else">
-                  Something Else
-                </option>
-              </select>
+
+          <div>
+            <div class="bg-white p-4 rounded-lg">
+              <div class="relative bg-inherit">
+                <div
+                  className="flex flex-col p-2 h-10 lg:w-72 w-full rounded-xl text-black placeholder-transparent ring-2 px-2 ring-gray-700 focus:ring-sky-600 focus:outline-none focus:border-rose-600 transition-all"
+                >
+                  <select
+                    onChange={(e) =>
+                      setContactDetails({
+                        ...ContactDetails,
+                        [e.target.name]: e.target.value,
+                      })
+                    }
+                    id="subject"
+                    name="subject"
+                    className=" text-black"
+                  >
+                    <option className="text-black hidden" value="">
+                      <span style={{ color: '#f5eacb' }}>How can we Help ?</span>
+                    </option>
+
+                    <option className="text-black" value="About Service">
+                      About Service
+                    </option>
+                    <option className="text-black" value="Technical Question">
+                      Technical Question
+                    </option>
+                    <option className="text-black" value="Career">
+                      Career
+                    </option>
+                    <option className="text-black" value="Something Else">
+                      Something Else
+                    </option>
+                  </select>
+                </div>
+              </div>
             </div>
             {error.subject ? (
               <div className="text-red-500 text-xs">{error.subject}</div>
@@ -170,17 +211,20 @@ const ContactForm = () => {
         <div className="flex flex-col">
           {Textarea("message", error.message)}
         </div>
+
         <ReCAPTCHA
           ref={captch}
           sitekey={import.meta.env.VITE_RECAPTCHA_KEY}
           onChange={(value) => setCaptchaValue(value)}
+          className="lg:ml-[230px]"
         />
         <button
           type="submit"
-          className="md:w-[30%] w-full bg-violet-500 transition-all duration-150 ease-linear hover:bg-white hover:text-violet-700 font-semibold shadow-[5px_5px_10px_3px] shadow-violet-900 text-white px-4 py-2 rounded-md mt-4"
+          className="md:w-[30%] w-full bg-violet-700 transition-all duration-150 ease-linear hover:bg-violet-900  hover:text-white font-semibold text-white px-4 py-2 mt-4 rounded-2xl lg:ml-[250px]"
         >
           {isPending ? "sending..." : "Send Message"}
         </button>
+
       </form>
     </>
   );
