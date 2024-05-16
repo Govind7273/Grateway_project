bf99638844c2a2219234b9664b806a50c264113c
Asmita <Asmitadhole@vionsys.com>
2024-05-16 19:03:06 +0530

    worked on responsiveness of IT Service Support page & did some changes

--- src/components/Services/ServiceSupport.jsx
+++ src/components/Services/ServiceSupport.jsx
@@ -85,82 +85,87 @@ const ServiceSupport = ({ Meta_Data }) => {
         <link rel="canonical" href={link} />
       </Helmet>
       {/* hero section */}
-      <section className="section relative w-full h-screen md:h-[40rem] overflow-hidden pharma_section bg-black">
+      <section className="relative w-full h-full overflow-hidden pharma_section bg-black">
         {/* first */}
         <div className="pharma_img md:w-[100%] md:h-auto pt-20 opacity-45">
           <img className="h-full w-full" src={bg} alt="support_bg" />
         </div>
         {/* second */}
-        <div className="pahrma_text absolute overflow-hidden top-12 w-full left-0 md:gap-5 gap-2 flex-col md:h-full h-[70%] flex justify-center ">
-          <h1 className="text-center md:text-TopHeading text-TopHeading-sm font-extrabold">
+        <div className="pahrma_text absolute overflow-hidden top-12 w-full left-0 md:gap-5 gap-2 flex-col h-full flex justify-center ">
+          <h1 className="text-center md:text-TopHeading text-2xl font-extrabold text-[#06b6d4]">
             Welcome to GreateWay Support
           </h1>
-          <p className="md:text-xl text-lg px-4 text-white text-center font-bold">
+          <p className="md:text-xl text-base px-4 text-white text-center font-bold">
             Require assistance? Our support team is ready to help you!
           </p>
         </div>
       </section>
 
       {/* second section */}
-      <div className="flex flex-wrap relative -top-48 md:top-0  justify-center items-center gap-10 md:p-5">
+      <section>
+      <div className="flex flex-wrap top-3 md:top-0 justify-center items-center gap-10 md:p-5 pt-2">
         {Supports.map((s) => {
           return (
-            <span key={s.id} className="items-center flex flex-col">
-              {<s.logo size={50} />}
-              <h1>{s.text}</h1>
-            </span>
+            <div>
+              <h1></h1>
+              <span key={s.id} className="items-center flex flex-col">
+                {<s.logo size={50} />}
+                <h1>{s.text}</h1>
+              </span>
+            </div>
           );
         })}
       </div>
+      </section>
 
       {/* Third Section */}
       <section>
-  {/* Main Content */}
-  <div className="bg-white py-12">
-    <div className="mx-auto max-w-7xl p-6 lg:px-8 border-4">
-      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-        <div className="flex flex-col items-center">
-          <h2 className="text-lg font-semibold leading-8 tracking-tight text-start text-indigo-600">Everything you need</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-start text-gray-900 sm:text-4xl">IT Service Support Overview</p>
-          <p className="mt-6 text-base leading-7 text-gray-600">Our comprehensive IT service support plays a pivotal role in upholding the reliability, security, and efficiency of organizational IT infrastructure. We tailor our services to accommodate the unique needs and scale of each client's business.</p>
-          <img src={listImg} alt="" />
-        </div>
-        <div className="grid grid-cols-1 gap-y-6 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:gap-y-2">
-          {/* List Items */}
-          {[
-            { title: "Help Desk Support", content: "Providing frontline assistance for users facing IT issues, including troubleshooting software and hardware problems and offering guidance on application usage." },
-            { title: "Technical Support", content: "Resolving complex technical challenges related to network issues, server management and system administration." },
-            { title: "Remote Monitoring and Management", content: "Proactively monitoring IT infrastructure remotely, performing automated maintenance and responding to alerts promptly." },
-            { title: "Incident Management and Resolution", content: "Logging and categorizing incidents, implementing escalation procedures for unresolved issues and conducting root cause analysis." },
-            { title: "Patch Management", content: "Applying patches, updates and fixes to keep systems secure and optimize performance, while ensuring compatibility and stability through testing." },
-            { title: "Backup and Disaster Recovery", content: "Implementing backup solutions and disaster recovery plans to protect data and ensure business continuity in case of data loss or system failure." }
-          ].map((item, index) => (
-            <div key={index} className="relative pl-9">
-              <div className="font-semibold text-gray-900">
-                <svg className="absolute top-1 left-0 h-5 w-5 text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd"></path>
-                </svg>
-                {item.title}
+        {/* Main Content */}
+        <div className="bg-white py-12">
+          <div className="mx-auto max-w-7xl p-6 lg:px-8 border-4">
+            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+              <div className="flex flex-col items-center">
+                <h2 className="text-lg font-semibold leading-8 tracking-tight text-start text-indigo-600">Everything you need</h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-start text-gray-900 sm:text-4xl">IT Service Support Offerings</p>
+                <p className="mt-6 text-base leading-7 text-gray-600">Our comprehensive IT service support plays a pivotal role in upholding the reliability, security, and efficiency of organizational IT infrastructure. We tailor our services to accommodate the unique needs and scale of each client's business.</p>
+                <img src={listImg} alt="" />
               </div>
-              <div className="mt-2">{item.content}</div>
+              <div className="grid grid-cols-1 gap-y-6 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:gap-y-2">
+                {/* List Items */}
+                {[
+                  { title: "Help Desk Support", content: "Providing frontline assistance for users facing IT issues, including troubleshooting software and hardware problems and offering guidance on application usage." },
+                  { title: "Technical Support", content: "Resolving complex technical challenges related to network issues, server management and system administration." },
+                  { title: "Remote Monitoring and Management", content: "Proactively monitoring IT infrastructure remotely, performing automated maintenance and responding to alerts promptly." },
+                  { title: "Incident Management and Resolution", content: "Logging and categorizing incidents, implementing escalation procedures for unresolved issues and conducting root cause analysis." },
+                  { title: "Patch Management", content: "Applying patches, updates and fixes to keep systems secure and optimize performance, while ensuring compatibility and stability through testing." },
+                  { title: "Backup and Disaster Recovery", content: "Implementing backup solutions and disaster recovery plans to protect data and ensure business continuity in case of data loss or system failure." }
+                ].map((item, index) => (
+                  <div key={index} className="relative pl-9">
+                    <div className="font-semibold text-gray-900">
+                      <svg className="absolute top-1 left-0 h-5 w-5 text-indigo-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd"></path>
+                      </svg>
+                      {item.title}
+                    </div>
+                    <div className="mt-2">{item.content}</div>
+                  </div>
+                ))}
+              </div>
             </div>
-          ))}
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
+      </section>
 
 
       {/* Accordion for FAQ */}
       <section>
-        <div className="p-2 m-4 md:m-8 relative md:top-0 -top-36 ">
+        <div className="p-2 m-4 md:m-4 relative md:top-5 top-0">
           <h2 className="md:text-SubHeading text-SubHeading-sm font-bold p-2 text-center  text-black">
             Have questions? We have answers! Check out our FAQ
           </h2>
-          {accordions.map((accordion) => (
+          {accordions.map((accordion,index) => (
             <Accordion
-              key={accordion.key}
+              key={index}
               title={accordion.title}
               data={accordion.data}
               isOpen={accordion.isOpen}
@@ -170,8 +175,8 @@ const ServiceSupport = ({ Meta_Data }) => {
         </div>
       </section>
 
-      <div className="flex relative md:top-0 -top-36 mb-[-80px] md:mb-0 h-[20vh] md:h-full justify-center w-full items-center gap-6 flex-col md:fl-row md:gap-6 p-3 md:p-5">
-        <h1 className=" md:text-MainHeading text-MainHeading-sm font-extrabold justify-center flex">
+      <div className="flex relative top-0 md:-top-36 md:mb-[-80px] mb-0 h-[30vh] md:h-full justify-center w-full items-center gap-6 flex-col md:fl-row md:gap-6 p-3 md:p-5">
+        <h1 className=" md:text-MainHeading text-MainHeading-sm font-extrabold justify-center flex text-center">
           Need more Assistant
         </h1>
         <button className="p-3 w-[70%] text-white md:w-fit bg-blue-500 hover:bg-blue-600 transition-all ease-linear font-bold rounded-xl">
