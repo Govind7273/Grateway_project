66ee9dc3b6e777782422892c9efefc37cf5420d3
RamanPatil68 <Ramanpatil@vionsys.com>
2024-07-15 17:59:19 +0530

    New MOU added

--- src/functions/Mou_Data.js
+++ src/functions/Mou_Data.js
@@ -2,6 +2,7 @@ import MarathWadaCLG from "../components/Mou/Images/MarathWadaCLG.jpg";
 import PuneVidyCLG from "../components/Mou/Images/PuneVidyCLG.jpg";
 import ramChandraCLG from "../components/Mou/Images/ramChandraCLG.jpg";
 import DholePatil from "../components/Mou/Images/DholePatil.jpg";
+import SKN from "../components/Mou/Images/SKN.jpg";
 
 
 export const textItems = [
@@ -34,6 +35,14 @@ export const textItems = [
       images: [
         {
           src: DholePatil,
+        }
+      ],
+    },
+    {
+      text: "SKN Sinhgad Institute of Technology & Science, Lonavala",
+      images: [
+        {
+          src: SKN,
         }
       ],
     }
